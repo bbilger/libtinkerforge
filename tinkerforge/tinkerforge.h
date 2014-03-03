4de6259d77375e1f09f31f2efecbc822454813de
Bjoern Bilger <bjoern.bilger@googlemail.com>
2014-03-03 21:36:50 +0100

    added tinkerforge.h

---
+++ tinkerforge/tinkerforge.h
@@ -0,0 +1,70 @@
+/*    
+ *    Copyright (C) 2014 Björn Bilger
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef TINKERFORGE_H_
+#define TINKERFORGE_H_
+
+#include <tinkerforge/bricklet_voltage_current.h>
+#include <tinkerforge/brick_dc.h>
+#include <tinkerforge/bricklet_current25.h>
+#include <tinkerforge/bricklet_humidity.h>
+#include <tinkerforge/bricklet_joystick.h>
+#include <tinkerforge/bricklet_motion_detector.h>
+#include <tinkerforge/bricklet_rotary_poti.h>
+#include <tinkerforge/bricklet_voltage.h>
+#include <tinkerforge/brick_imu.h>
+#include <tinkerforge/bricklet_distance_ir.h>
+#include <tinkerforge/bricklet_industrial_digital_in_4.h>
+#include <tinkerforge/bricklet_lcd_16x2.h>
+#include <tinkerforge/bricklet_multi_touch.h>
+#include <tinkerforge/bricklet_segment_display_4x7.h>
+#include <tinkerforge/brick_master.h>
+#include <tinkerforge/bricklet_ambient_light.h>
+#include <tinkerforge/bricklet_distance_us.h>
+#include <tinkerforge/bricklet_industrial_digital_out_4.h>
+#include <tinkerforge/bricklet_lcd_20x4.h>
+#include <tinkerforge/bricklet_piezo_buzzer.h>
+#include <tinkerforge/bricklet_sound_intensity.h>
+#include <tinkerforge/brick_servo.h>
+#include <tinkerforge/bricklet_analog_in.h>
+#include <tinkerforge/bricklet_dual_button.h>
+#include <tinkerforge/bricklet_industrial_dual_0_20ma.h>
+#include <tinkerforge/bricklet_led_strip.h>
+#include <tinkerforge/bricklet_piezo_speaker.h>
+#include <tinkerforge/bricklet_temperature.h>
+#include <tinkerforge/brick_stepper.h>
+#include <tinkerforge/bricklet_analog_out.h>
+#include <tinkerforge/bricklet_dual_relay.h>
+#include <tinkerforge/bricklet_industrial_quad_relay.h>
+#include <tinkerforge/bricklet_linear_poti.h>
+#include <tinkerforge/bricklet_ptc.h>
+#include <tinkerforge/bricklet_temperature_ir.h>
+#include <tinkerforge/ip_connection.h>
+#include <tinkerforge/bricklet_barometer.h>
+#include <tinkerforge/bricklet_gps.h>
+#include <tinkerforge/bricklet_io16.h>
+#include <tinkerforge/bricklet_line.h>
+#include <tinkerforge/bricklet_remote_switch.h>
+#include <tinkerforge/bricklet_tilt.h>
+#include <tinkerforge/bricklet_current12.h>
+#include <tinkerforge/bricklet_hall_effect.h>
+#include <tinkerforge/bricklet_io4.h>
+#include <tinkerforge/bricklet_moisture.h>
+#include <tinkerforge/bricklet_rotary_encoder.h>
+
+#endif // TINKERFORGE_H_
